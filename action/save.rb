57b720e76417c99bd6ef34ec0a3980e8d7efc264
GitTaro000000 <sample@mail.com>
2021-07-20 15:10:50 +0900

    :bug:fix save function

--- action/save.rb
+++ action/save.rb
@@ -4,6 +4,11 @@ File.open("./action/.save", "r+", encoding: "utf-8") do |file|
   @sto = file.read
   @save = @sto.to_i
   File.open("./action/.save", "w+") do |f|
+    if @save == 0
+      @save = 1
+    elsif @save >= 11
+      @save = 1
+    end
     f.puts @save.to_i
     @load = @save.to_i
     puts "セーブを完了しました。次回は#{@load}を開きます。"
@@ -12,6 +17,5 @@ end
 
 # オフラインきつつきが終わった後に@autoSaveを1にする。 +++
 # -----------------------------------------------------
-
 $story = @load
 @hakai = @load
